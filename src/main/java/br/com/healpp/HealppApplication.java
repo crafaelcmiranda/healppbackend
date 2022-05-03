e4a11402f40c7fb99d999c6b355d86a3792a4e59
Raafel Miranda <ext.carlos.miranda@dafiti.com.br>
2022-05-03 11:33:08 -0300

    healpp initial project

---
+++ src/main/java/br/com/healpp/HealppApplication.java
@@ -0,0 +1,13 @@
+package br.com.healpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealppApplication.class, args);
+	}
+
+}
